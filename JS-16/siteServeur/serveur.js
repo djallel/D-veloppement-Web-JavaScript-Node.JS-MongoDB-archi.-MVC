ee8cc75748fe23074ee7bd15ebf6cce0dd3288f7
TABET DJALAL <b_tabet@esi.dz>
2020-06-30 01:26:32 +0200

    1ere route

--- JS-16/siteServeur/serveur.js
+++ JS-16/siteServeur/serveur.js
@@ -3,8 +3,17 @@ var server = express();
 
 server.listen(3000);
 
-server.use("/", function(requete, reponse){
-    console.log("Reception d'un demande cliente");
-    console.log(requete.path);
-    reponse.end("bonjour Client");
+server.get("/", (requete, reponse) =>{
+    console.log("demande recue avec la méthode GET sur l'url /")
+    reponse.end("Demande GET reçue")
+})
+
+server.get("/test", (requete, reponse) =>{
+    console.log("demande recue avec la méthode GET sur l'url /test")
+    reponse.end("Demande GET reçue")
+})
+
+server.post("/test", (requete, reponse) =>{
+    console.log("demande recue avec la méthode POST sur l'url /test")
+    reponse.end("Demande POST reçue")
 })
