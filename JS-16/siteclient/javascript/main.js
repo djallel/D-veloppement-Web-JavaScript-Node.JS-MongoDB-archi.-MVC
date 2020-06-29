ba2df63f0367e12fee11b1d996ee37052edd9db0
TABET DJALAL <b_tabet@esi.dz>
2020-06-29 23:05:40 +0200

    Ajout d'un livres.html+ refraichissement

--- JS-16/siteclient/javascript/main.js
+++ JS-16/siteclient/javascript/main.js
@@ -21,12 +21,12 @@ var l4 = {
     pages : 120
 }
 
-var livres = [l1,l2,l3,l4];
-for (i = 0 ; i <= livres.length -1 ; i++){
+var biblio = [l1,l2,l3,l4];
+for (i = 0 ; i <= biblio.length -1 ; i++){
     var livre1 = document.createElement("tr");
-    livre1.innerHTML = `<td>${livres[i].nom}</td>
-                    <td>${livres[i].auteur}</td>
-                    <td>${livres[i].pages}</td>
+    livre1.innerHTML = `<td>${biblio[i].nom}</td>
+                    <td>${biblio[i].auteur}</td>
+                    <td>${biblio[i].pages}</td>
                     <td><button class="btn btn-warning">Modifier</button></td>
                     <td><button class="btn btn-danger">Supprimer</button></td>`;
     table.appendChild(livre1);
@@ -34,5 +34,23 @@ for (i = 0 ; i <= livres.length -1 ; i++){
 
 function ajoutFormulaire(){
     document.querySelector("#ajoutForm").removeAttribute("class");
+}
+
+document.querySelector("#validationFormAjout").addEventListener("click", function(event){
+    event.preventDefault();
+    var titre = document.querySelector("#ajoutForm #titre").value;
+    var auteur = document.querySelector("#ajoutForm #auteur").value;
+    var pages = document.querySelector("#ajoutForm #pages").value;
+    ajoutLivre(titre,auteur,pages);
+});
+
+function ajoutLivre(titre,auteur,pages){
+    var livre = {
+        nom : titre,
+        auteur : auteur,
+        pages : pages
+    }
+    biblio.push(livre);
+    console.log(biblio);
 }
 
