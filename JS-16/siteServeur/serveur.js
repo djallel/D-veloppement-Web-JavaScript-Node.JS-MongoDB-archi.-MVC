fe326a4c0225db665f210b482a5a85d357376424
TABET DJALAL <b_tabet@esi.dz>
2020-06-30 23:59:28 +0200

    Gestion des erreurs 404

--- JS-16/siteServeur/serveur.js
+++ JS-16/siteServeur/serveur.js
@@ -20,3 +20,14 @@ server.post("/test", (requete, reponse) =>{
     console.log("demande recue avec la méthode POST sur l'url /test")
     reponse.end("Demande POST reçue")
 })
+
+server.use((requete,reponse,suite) => {
+    const error = new Error("Page non trouvée");
+    error.status= 404;
+    suite(error);
+})
+
+server.use((error,requete,reponse) => {
+    reponse.status(error.status || 500);
+    reponse.end(error.message);
+})
