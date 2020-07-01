df2c50d794643509e256bdb0f05f68c92c6ceac4
TABET DJALAL <b_tabet@esi.dz>
2020-07-01 02:36:47 +0200

    creation bd

--- JS-16/siteServeur/serveur.js
+++ JS-16/siteServeur/serveur.js
@@ -1,7 +1,25 @@
-var express = require("express");
-var server = express();
-var morgan = require("morgan");
-var router = require("./routeur");
+const express = require("express");
+const server = express();
+const morgan = require("morgan");
+const router = require("./routeur");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/biblio2", {useNewUrlParser:true,useUnifiedTopology:true});
+
+const livreSchema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    nom: String,
+    auteur: String,
+    pages : Number
+})
+const livreModel = mongoose.model("Livre",livreSchema);
+
+livreModel.find()
+    .exec()
+    .then(livres => {
+        console.log(livres)
+    })
+    .catch();
 
 server.use(express.static("public"))
 server.use(morgan("dev"));
